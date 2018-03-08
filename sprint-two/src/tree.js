5efcacf97008362df13585c09c1318aa1fd6ad2c
Raymond Cabrera <rjgcengr@gmail.com>
2018-03-07 18:34:32 -0800

    Implement tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,47 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];
+  extend(newTree, treeMethods);
   return newTree;
 };
 
-var treeMethods = {};
-
-treeMethods.addChild = function(value) {
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
-treeMethods.contains = function(target) {
+this.treeMethods = {};
+
+this.treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
+};
+
+this.treeMethods.contains = function(target) {
+  var result = false;
+  if(this.value === target){
+    return true;
+  }
+  var searchChildren = function(child){
+    for(var i = 0; i < child.children.length; i++){
+      if(child.children[i].value === target){
+        result = true;
+      }
+      if(child.children[i].children.length > 0){
+        searchChildren(child.children[i]);
+      }
+    }
+  }
+  searchChildren(this);
+
+  return result;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addChild: O(1)
+ * contains: O(n)
  */
