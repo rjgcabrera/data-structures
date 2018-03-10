d120fb73e1a56e2ad5b91729850b51d0fb043984
Raymond Cabrera <rjgcengr@gmail.com>
2018-03-09 19:29:59 -0800

    Implemented resizing for HashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,21 +2,54 @@
 
 var HashTable = function() {
   this._limit = 8;
+  this._min = 8;
   this._storage = LimitedArray(this._limit);
+  this.count = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, [[k, v]]);
+    this.count++;
   } else {
     for (var i = 0; i < this._storage.get(index).length; i++) {
       if (k === this._storage.get(index)[i][0]) {
         this._storage.get(index)[i][1] = v;
+        this.count++;
         return;
       }
     }
+    this.count++;
     this._storage.get(index).push([k,v]);
+  }
+
+  if (this.count >= .75 * this._limit) {
+    this.count = 0;
+    this._limit *= 2;
+    var tempStorage = LimitedArray(this._limit);
+
+    this._storage.each((bucket) => {
+      if (bucket !== undefined) {
+        for (var bucketIdx = 0; bucketIdx < bucket.length; bucketIdx++) {
+          var newKey = bucket[bucketIdx][0];
+          var newVal = bucket[bucketIdx][1];
+          var index = getIndexBelowMaxForKey(newKey, this._limit);
+          if (tempStorage.get(index) === undefined) {
+            tempStorage.set(index, [[newKey, newVal]]);
+          } else {
+            for (var i = 0; i < tempStorage.get(index).length; i++) {
+              if (newKey === tempStorage.get(index)[i][0]) {
+                tempStorage.get(index)[i][1] = newVal;
+                return;
+              }
+            }
+            tempStorage.get(index).push([newKey, newVal]);
+          }
+        }
+      }
+    });
+    this._storage = tempStorage;
   }
 };
 
@@ -38,9 +71,42 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
-};
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (k === this._storage.get(index)[i][0]) {
+        this._storage.get(index).splice(i, 1);
+        this.count--;
+      }
+    }
+  if (this.count < .25 * this._limit) {
+    this._limit = this._limit / 2;
+    if(this._limit < this._min) {
+      this._limit = this._min;
+    }
+    var tempStorage = LimitedArray(this._limit);
 
+    this._storage.each((bucket) => {
+      if (bucket !== undefined) {
+        for (var bucketIdx = 0; bucketIdx < bucket.length; bucketIdx++) {
+          var newKey = bucket[bucketIdx][0];
+          var newVal = bucket[bucketIdx][1];
+          var index = getIndexBelowMaxForKey(newKey, this._limit);
+          if (tempStorage.get(index) === undefined) {
+            tempStorage.set(index, [[newKey, newVal]]);
+          } else {
+            for (var i = 0; i < tempStorage.get(index).length; i++) {
+              if (newKey === tempStorage.get(index)[i][0]) {
+                tempStorage.get(index)[i][1] = newVal;
+                return;
+              }
+            }
+            tempStorage.get(index).push([newKey, newVal]);
+          }
+        }
+      }
+    });
+    this._storage = tempStorage;
+  }
+};
 
 
 /*
